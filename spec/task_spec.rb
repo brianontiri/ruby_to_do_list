277aa0aeef2c0eed7a2c3060caa0d428df91eade
brianontiri <foughtsentertainment@gmail.com>
2017-07-10 15:51:37 +0300

    required 'pgp in task spec

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,5 +1,14 @@
 require('rspec')
+ require("pg")
   require('task')
+
+  DB = PG.connect({:dbname => 'to_do_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *;")
+  end
+end
 
   describe(Task) do
     before() do
