9153b195f1de3fa6d570e1172e167704d0baa4a7
brianontiri <foughtsentertainment@gmail.com>
2017-07-10 15:52:20 +0300

    required 'pgp in taapp.rbsk spec

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require("sinatra")
   require("sinatra/reloader")
   also_reload("lib/**/*.rb")
   require("./lib/task")
-  require("pg")
+  require("pg") #
 
   DB = PG.connect({:dbname => "to_do"})
 
